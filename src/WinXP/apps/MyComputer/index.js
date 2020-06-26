30997f844c1e2dbe263dcecbd40de4e2ddf6e959
Thomas Milburn <milburn.j.thomas@gmail.com>
2020-06-26 19:18:01 +0100

    Changed my computer

--- src/WinXP/apps/MyComputer/index.js
+++ src/WinXP/apps/MyComputer/index.js
@@ -221,59 +221,33 @@ function MyComputer({ onClose }) {
               </div>
               <div className="com__content__left__card__content">
                 <div className="com__content__left__card__row">
-                  <iframe
-                    title="ghbtn"
-                    style={{ margin: '0 0 3px -1px', height: '30px' }}
-                    src="https://ghbtns.com/github-btn.html?user=ShizukuIchi&repo=winXP&type=star&count=true&size=large"
-                    frameBorder="0"
-                    scrolling="0"
-                    width="170px"
-                    height="20px"
-                  />
-                </div>
-                <div className="com__content__left__card__row">
                   <img
                     className="com__content__left__card__img"
-                    src="https://cdn.iconscout.com/icon/free/png-256/medium-1425876-1205067.png"
+                    src={logo}
                     alt="control"
                   />
                   <ga.OutboundLink
-                    eventLabel="https://medium.com/@ShizukuIchi"
-                    to="https://medium.com/@ShizukuIchi"
+                    eventLabel="https://github.com/TJM4/Personal-Site"
+                    to="https://github.com/TJM4/Personal-Site"
                     className="com__content__left__card__text link"
                     target="_blank"
                   >
-                    Medium
+                    View on GitHub
                   </ga.OutboundLink>
                 </div>
                 <div className="com__content__left__card__row">
                   <img
                     className="com__content__left__card__img"
-                    src="https://image.flaticon.com/icons/png/128/179/179312.png"
+                    src={logo}
                     alt="control"
                   />
                   <ga.OutboundLink
-                    eventLabel="https://sh1zuku.csie.io/blog"
-                    to="https://sh1zuku.csie.io/blog"
+                    eventLabel="https://github.com/ShizukuIchi/winXP"
+                    to="https://github.com/ShizukuIchi/winXP"
                     className="com__content__left__card__text link"
                     target="_blank"
                   >
-                    Blog
-                  </ga.OutboundLink>
-                </div>
-                <div className="com__content__left__card__row">
-                  <img
-                    className="com__content__left__card__img"
-                    src={mine}
-                    alt="control"
-                  />
-                  <ga.OutboundLink
-                    eventLabel="https://github.com/ShizukuIchi/minesweeper"
-                    to="https://github.com/ShizukuIchi/minesweeper"
-                    className="com__content__left__card__text link"
-                    target="_blank"
-                  >
-                    Minesweeper(mobile)
+                    Forked from winXP
                   </ga.OutboundLink>
                 </div>
               </div>
@@ -347,41 +321,6 @@ function MyComputer({ onClose }) {
                     CD Drive (D:)
                   </div>
                 </div>
-              </div>
-            </div>
-            <div className="com__content__right__card com__content__right__card--me">
-              <div className="com__content__right__card__header">
-                About Me :)
-              </div>
-              <div className="com__content__right__card__content">
-                <ga.OutboundLink
-                  eventLabel="https://github.com/ShizukuIchi"
-                  to="https://github.com/ShizukuIchi"
-                  className="com__content__right__card__item--me"
-                  target="_blank"
-                >
-                  <img
-                    className="com__content__right__card__img"
-                    src={logo}
-                    alt="control"
-                  />
-                  <div className="com__content__right__card__text">Github</div>
-                </ga.OutboundLink>
-                <ga.OutboundLink
-                  eventLabel="https://sh1zuku.csie.io"
-                  to="https://sh1zuku.csie.io"
-                  className="com__content__right__card__item--me"
-                  target="_blank"
-                >
-                  <img
-                    className="com__content__right__card__img"
-                    src="https://a.ppy.sh/2926513_1448497605.png"
-                    alt="control"
-                  />
-                  <div className="com__content__right__card__text">
-                    My Website
-                  </div>
-                </ga.OutboundLink>
               </div>
             </div>
           </div>
